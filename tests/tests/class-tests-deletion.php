2fd6174988d1dc71fa7995e827099026326d40c5
Akihiro Harai <jharai0815@gmail.com>
2021-02-26 12:28:29 +0900

    Delete job even if it is currently running #11

--- tests/tests/class-tests-deletion.php
+++ tests/tests/class-tests-deletion.php
@@ -24,22 +24,46 @@ class Tests_Deletion extends WP_UnitTestCase {
 		global $wpdb;
 
 		// Schedule an event and make sure it's returned by wp_next_scheduled().
-		$hook      = __FUNCTION__;
 		$timestamp = strtotime( '+1 hour' );
 
-		wp_schedule_single_event( $timestamp, $hook );
+		wp_schedule_single_event( $timestamp, __FUNCTION__ );
 		$before = time();
 		sleep( 1 );
-		wp_unschedule_event( $timestamp, $hook );
+		wp_unschedule_event( $timestamp, __FUNCTION__ );
 		sleep( 1 );
 		$after = time();
 
-		$sql          = "SELECT * FROM {$wpdb->base_prefix}cavalcade_jobs WHERE hook = %s";
-		$sql_params[] = $hook;
+		$results = $wpdb->get_results( $wpdb->prepare(
+			"SELECT * FROM {$wpdb->base_prefix}cavalcade_jobs WHERE hook = %s",
+			[ __FUNCTION__ ] ) );
+		$t = mysql2date( 'G', $results[0]->deleted_at );
 
-		$query   = $wpdb->prepare( $sql, $sql_params );
-		$results = $wpdb->get_results( $query );
-		$t       = mysql2date( 'G', $results[0]->deleted_at );
+		$this->assertGreaterThan( $before, $t );
+		$this->assertLessThan( $after, $t );
+	}
+
+	function test_unschedule_event_while_running() {
+		global $wpdb;
+
+		// Schedule an event and make sure it's returned by wp_next_scheduled().
+		$timestamp = strtotime( '+1 hour' );
+
+		wp_schedule_single_event( $timestamp, __FUNCTION__ );
+		$before = time();
+		sleep( 1 );
+		// status transition: waiting -> running 
+		$wpdb->query( $wpdb->prepare(
+			"UPDATE {$wpdb->base_prefix}cavalcade_jobs SET status = 'running' WHERE hook = %s",
+			[ __FUNCTION__ ] ) );
+		
+		wp_unschedule_event( $timestamp, __FUNCTION__ );
+		sleep( 1 );
+		$after = time();
+
+		$results = $wpdb->get_results( $wpdb->prepare(
+			"SELECT * FROM {$wpdb->base_prefix}cavalcade_jobs WHERE hook = %s",
+			[ __FUNCTION__ ] ) );
+		$t = mysql2date( 'G', $results[0]->deleted_at );
 
 		$this->assertGreaterThan( $before, $t );
 		$this->assertLessThan( $after, $t );
@@ -48,28 +72,25 @@ class Tests_Deletion extends WP_UnitTestCase {
 	function test_clear_schedule() {
 		global $wpdb;
 
-		$hook = __FUNCTION__;
-		$args = array( 'arg1' );
-
 		// Schedule several events with and without arguments.
-		wp_schedule_single_event( strtotime( '+1 hour' ), $hook );
-		wp_schedule_single_event( strtotime( '+2 hour' ), $hook );
-		wp_schedule_single_event( strtotime( '+3 hour' ), $hook, $args );
-		wp_schedule_single_event( strtotime( '+4 hour' ), $hook, $args );
+		wp_schedule_single_event( strtotime( '+1 hour' ), __FUNCTION__ );
+		wp_schedule_single_event( strtotime( '+2 hour' ), __FUNCTION__ );
+		wp_schedule_single_event( strtotime( '+3 hour' ), __FUNCTION__, array( 'arg1' ) );
+		wp_schedule_single_event( strtotime( '+4 hour' ), __FUNCTION__, array( 'arg1' ) );
 
 		$before = time();
 		sleep( 1 );
 
 		// Clear the schedule for the no args events and make sure it's gone.
-		$hook_unscheduled = wp_clear_scheduled_hook( $hook );
+		$hook_unscheduled = wp_clear_scheduled_hook( __FUNCTION__ );
 
 		sleep( 1 );
 		$after = time();
 
-		$sql          = "SELECT * FROM {$wpdb->base_prefix}cavalcade_jobs WHERE hook = %s AND deleted_at IS NOT NULL";
-		$sql_params[] = $hook;
+		$query = $wpdb->prepare(
+			"SELECT * FROM {$wpdb->base_prefix}cavalcade_jobs WHERE hook = %s AND deleted_at IS NOT NULL",
+			[ __FUNCTION__ ] );
 
-		$query   = $wpdb->prepare( $sql, $sql_params );
 		$results = $wpdb->get_results( $query );
 
 		$this->assertCount( 2, $results );
@@ -84,8 +105,8 @@ class Tests_Deletion extends WP_UnitTestCase {
 
 		// Clear the schedule for the args events and make sure they're gone too.
 		// Note: wp_clear_scheduled_hook() expects args passed directly, rather than as an array.
-		wp_clear_scheduled_hook( $hook, $args );
-		$this->assertFalse( wp_next_scheduled( $hook, $args ) );
+		wp_clear_scheduled_hook( __FUNCTION__, array( 'arg1' ) );
+		$this->assertFalse( wp_next_scheduled( __FUNCTION__, array( 'arg1' ) ) );
 
 		$this->assertCount( 4, $wpdb->get_results( $query ) );
 	}
